9b2703baf20dbfb39192da0977a84ad3251a9eb0
jooprima <jooprima@gmail.com>
2020-03-16 23:37:57 +0700

    appBar

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,15 @@ class HomePage extends StatelessWidget{
 
   Widget build(BuildContext context){
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        leading: Icon(Icons.home),
+        title: Center(child: Text("jooprimApp") ,) ,
+        actions: <Widget>[
+          Icon(Icons.search),
+          Icon(Icons.alarm)
+        ],
+      ),
       body: Container(
         color: Colors.red[700],
         width: 400.0,
